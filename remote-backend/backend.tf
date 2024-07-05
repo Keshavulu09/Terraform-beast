5f771a4687f877ed3e5ca7bca805732fd4659152
Keshavulu09 <140239390+Keshavulu09@users.noreply.github.com>
2024-07-05 11:55:34 +0530

    Update backend.tf

--- remote-backend/backend.tf
+++ remote-backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
+    bucket         = "keshav-s3-backed"
+    key            = "keshav/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
